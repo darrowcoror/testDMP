178756f9dfa7ccc1f9c93a40f2c2e18afa093df8
Marisa Strong <marisa.strong@ucop.edu>
2011-06-02 14:01:41 -0700

    initial commit of dmpTool app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DmpTool::Application.config.secret_token = '054f831e6bca93b66a0f15323ccc11455a4cb383deaa33d6781ed20b8ac56d4a1adc8e6ebb1435bd3bfe403e4fe9d26289ab93d438a430903a6483621d74c514'
