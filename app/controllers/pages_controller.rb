78e9ffb4a24c2990c29069518ccf647804d5672b
Marisa Strong <marisa.strong@ucop.edu>
2011-06-02 17:31:39 -0700

    added views for the pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def home
+     @title = "Home"
+  end
+
+  def contact
+     @title = "Contact Us"
+  end
+
+  def about
+     @title = "About Us"
+  end
+
+  def help
+     @title = "Help"
+  end
+
+end
